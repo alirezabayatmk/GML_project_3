553d0b873fb9b4c6bc887c763a6324d0eee2c944
Alireza Bayat Makou <alireza.slimshady@gmail.com>
2022-01-18 23:20:18 +0100

    changed column names from From,To to Source,Target to be able to import files in gephi

--- utils/node_id_mapper.py
+++ utils/node_id_mapper.py
@@ -7,14 +7,14 @@ def text_to_zeroed_csv(path):
     dataset_name = str(path.split('/')[-1]).replace('.txt','')
     
     df = pd.read_csv(path, skiprows=3, delimiter='\t')
-    df = df.rename(columns={'# FromNodeId':'From','ToNodeId':'To'})
+    df = df.rename(columns={'# FromNodeId':'Source','ToNodeId':'Target'})
     
-    G = nx.from_pandas_edgelist(df,'From','To')
+    G = nx.from_pandas_edgelist(df,'Source','Target')
     nodes = G.number_of_nodes()
     edges = G.number_of_edges()
     
-    from_list = list(df['From'].unique())
-    to_list = list(df['To'].unique())
+    from_list = list(df['Source'].unique())
+    to_list = list(df['Target'].unique())
     node_ids = list(set(list(from_list + to_list)))
 
     node_ids.sort()
@@ -26,8 +26,8 @@ def text_to_zeroed_csv(path):
     df_id = pd.DataFrame.from_dict(d.items(), orient='columns')
     df_id.columns=['main_node_id','mapped_node_id']
 
-    df['From'] = df['From'].map(d)
-    df['To'] = df['To'].map(d)
+    df['Source'] = df['Source'].map(d)
+    df['Target'] = df['Target'].map(d)
 
     df.to_csv('datasets/normalized_id/{}(corrected_index).csv'.format(dataset_name), index=False)
     df_id.to_csv('mapping_data/{}(mapped_id).csv'.format(dataset_name))
